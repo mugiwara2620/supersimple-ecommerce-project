dad529fbc80cdfbbc454708d7d9eafbe15ef8502
John Doe <ameaymaneelabba@gmail.com>
2025-10-11 07:48:37 +0200

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,14 @@ import { products } from '../../starting-code/data/products.js'
 
 
 export function HomePage() {
+	fetch('http://localhost:3000/api/products')
+	.then( (response)=>{
+		return response.json()
+	}).then((data)=>{
+			console.log(data);
+
+		})
+
 	function Product({ image, name, rating, priceCents }) {
 		return (
 			<div className="product-container">
